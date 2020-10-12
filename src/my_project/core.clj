2b83585e77732cb31fd9ba7d614724030f79a975
0918nobita <nobita.0918@gmail.com>
2020-10-12 18:50:24 +0900

    :recycle: doto マクロを用いて、メソッド呼び出し部分の可読性を向上させた

--- src/my_project/core.clj
+++ src/my_project/core.clj
@@ -18,27 +18,26 @@
      "java/lang/Object"
      nil)
     (let [init-method-visitor (.visitMethod cw (Main/ACC_PUBLIC) "<init>" "()V" nil nil)]
-      (.visitCode init-method-visitor)
-      (.visitVarInsn init-method-visitor (Main/ALOAD) 0)
-      (.visitMethodInsn init-method-visitor (Main/INVOKESPECIAL) "java/lang/Object" "<init>" "()V")
-      (.visitInsn init-method-visitor (Main/RETURN))
-      (.visitMaxs init-method-visitor 1 1)
-      (.visitEnd init-method-visitor))
+      (doto
+       init-method-visitor
+        (.visitCode)
+        (.visitVarInsn (Main/ALOAD) 0)
+        (.visitMethodInsn (Main/INVOKESPECIAL) "java/lang/Object" "<init>" "()V")
+        (.visitInsn (Main/RETURN))
+        (.visitMaxs 1 1)
+        (.visitEnd)))
     (let [main-method-visitor
           (.visitMethod cw (+ (Main/ACC_PUBLIC) (Main/ACC_STATIC)) "main" "([Ljava/lang/String;)V" nil nil)]
-      (.visitCode main-method-visitor)
-      (.visitFieldInsn main-method-visitor (Main/GETSTATIC) "java/lang/System" "out" "Ljava/io/PrintStream;")
-      (.visitLdcInsn main-method-visitor "Hello from ASM")
-      (.visitMethodInsn main-method-visitor
-                        (Main/INVOKEVIRTUAL)
-                        "java/io/PrintStream"
-                        "println"
-                        "(Ljava/lang/String;)V")
-      (.visitInsn main-method-visitor (Main/RETURN))
-      (.visitMaxs main-method-visitor 2 1)
-      (.visitEnd main-method-visitor))
+      (doto main-method-visitor
+        (.visitCode)
+        (.visitFieldInsn (Main/GETSTATIC) "java/lang/System" "out" "Ljava/io/PrintStream;")
+        (.visitLdcInsn "Hello from ASM")
+        (.visitMethodInsn (Main/INVOKEVIRTUAL) "java/io/PrintStream" "println" "(Ljava/lang/String;)V")
+        (.visitInsn (Main/RETURN))
+        (.visitMaxs 2 1)
+        (.visitEnd)))
     (.visitEnd cw)
-    (let [out (DataOutputStream. (FileOutputStream. "HelloFromASM.class"))]
-      (.write out (.toByteArray cw))
-      (.flush out)
-      (.close out))))
+    (doto (DataOutputStream. (FileOutputStream. "HelloFromASM.class"))
+      (.write (.toByteArray cw))
+      (.flush)
+      (.close))))
